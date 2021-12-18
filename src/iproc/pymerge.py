9691253fcc117dff71b83d966d16a24b445cfa5b
Joseph Diza <josephm.diza@gmail.com>
2021-12-18 15:44:17 -0800

    Concat two image files together

---
+++ src/iproc/pymerge.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+im1 = Image.open('image1.jpg')
+im2 = Image.open('image2.jpg')
+
+def get_concat_h(im1, im2):
+    dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
+def get_concat_v(im1, im2):
+    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+
+get_concat_h(im1, im2).save('output_h.jpg')
+get_concat_v(im1, im2).save('output_v.jpg')
