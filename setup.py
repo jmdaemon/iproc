dcda65b90bdce21859fb5e912d07c7f2d459f20d
Joseph Diza <josephm.diza@gmail.com>
2022-01-05 18:34:39 -0800

    Rename iproc pkg

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="iproc-jmd",
+    name="iproc",
     version="0.1.0",
     author="Joseph Diza",
     author_email="josephm.diza@gmail.com",
